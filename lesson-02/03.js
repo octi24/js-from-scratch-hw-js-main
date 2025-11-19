19d12de4c26f3a42ec9ab33bd7ac741ecbbe49cd
MaximYazikov <Forman3409@gmail.com>
2025-11-19 11:15:13 +0300

    lesson-02-03-complete

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,31 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+let score; // тестовое значение, можно изменять
+let grade;
+let count = 0;
 // your code
+
+while (count <= 5) {
+  score = Math.floor(Math.random() * 100) + 1;
+  console.log(score);
+  if (score <= 49) {
+    grade = 'F';
+    console.log(grade);
+  } else if (score >= 50 && score <= 69) {
+    grade = 'D';
+    console.log(grade);
+  } else if (score >= 70 && score <= 79) {
+    grade = 'C';
+    console.log(grade);
+  } else if (score >= 80 && score <= 89) {
+    grade = 'B';
+    console.log(grade);
+  } else if (score >= 90 && score <= 100) {
+    grade = 'D';
+    console.log(grade);
+  } else {
+    console.log('Hello');
+  }
+  count++;
+}
