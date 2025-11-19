bdda8b5e9ddbebc82afbc84431511b44c0a71635
MaximYazikov <Forman3409@gmail.com>
2025-11-19 10:31:27 +0300

    lesson-02-01-complete

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,21 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+const isAdmin = false;
+const isVerifiedUser = true;
+const hasSpecialPermission = true;
+const hasTemporaryPass = false;
 
-let isAccess
+let isAccess;
 
 // your code
+if (isAdmin && hasSpecialPermission) {
+  isAccess = true;
+  console.log(isAccess);
+} else if (isVerifiedUser && hasTemporaryPass) {
+  isAccess = true;
+  console.log(isAccess);
+} else {
+  isAccess = false;
+  console.log(isAccess);
+}
