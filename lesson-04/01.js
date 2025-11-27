87003166a30f2d83e021e73308ed68f621a25c59
MaximYazikov <Forman3409@gmail.com>
2025-11-27 16:38:51 +0300

    lesson-04-01-complete

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,17 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+const array = [12, 14, 55, 667, 91];
+const element = 54;
+
+function includesElement(arr, el) {
+  let indexElement = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === el) {
+      indexElement = true;
+      break;
+    }
+  }
+  return indexElement;
+}
+console.log(includesElement(array, element));
